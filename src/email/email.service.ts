0edacae11075c06497a1f20ceef452816fe88b7c
SantiagoMtz25 <santimtzv01@gmail.com>
2024-08-19 19:37:14 -0600

    email back

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import * as dotenv from 'dotenv';
 
+dotenv.config();
+
 @Injectable()
 export class EmailService {
   private transporter;
@@ -18,11 +20,14 @@ export class EmailService {
   }
 
   async sendContactMail(name: string, email: string, motive: string, message: string) {
+    let nameCap = name.charAt(0).toUpperCase() + name.slice(1);
+    let motiveCap = motive.charAt(0).toUpperCase() + motive.slice(1);
+    let messageCap = message.charAt(0).toUpperCase() + message.slice(1);
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: process.env.EMAIL_USER,
-      subject: `New Contact from ${name}`,
-      text: `Motive: ${motive}\n\nEmail: ${email}\n\nMessage: ${message}`,
+      subject: `New Contact from ${nameCap}`,
+      text: `Motive: ${motive}\n\nEmail: ${motiveCap}\n\nMessage: ${messageCap}`,
     };
 
     return await this.transporter.sendMail(mailOptions);
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true, 
+  });
   await app.listen(3000);
 }
 bootstrap();
